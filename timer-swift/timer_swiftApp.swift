e2ecba476b316215072ac7fb76ab5909312339b2
kondo takeshi <chaspy@users.noreply.github.com>
2023-09-03 14:05:16 +0900

    init

---
+++ timer-swift/timer_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  timer_swiftApp.swift
+//  timer-swift
+//
+//  Created by Takeshi Kondo on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct timer_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
