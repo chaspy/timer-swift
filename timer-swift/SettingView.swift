3ecc5c490edeaaabbeacb15441d9f69dc0ee60fb
kondo takeshi <chaspy@users.noreply.github.com>
2023-09-03 14:08:43 +0900

    feat: Add SettingView.swift to project

---
+++ timer-swift/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  timer-swift
+//
+//  Created by Takeshi Kondo on 2023/09/03.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
